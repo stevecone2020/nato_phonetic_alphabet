8aa2f5ad9e40bed499ac648b592b044335bc3798
stevecone2020 <63656442+stevecone2020@users.noreply.github.com>
2022-06-07 17:24:09 +0100

    second push

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ phonetic_dict = {row.letter : row.code for (index, row) in data.iterrows()}
 
 print(phonetic_dict)
 
-word = input("Please enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+
+def generate_phonetic():
+    word = input("Please enter a word: ").upper()
+
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("letters in the alphabet only!")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
+
